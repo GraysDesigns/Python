5e62918eee8f404864f46f2c6896f9b4caaab585
Richard Gray <richardgray@richards-air.lan>
2020-11-08 22:16:35 -0500

    updating movietheater

--- MovieTheaterGray.py
+++ MovieTheaterGray.py
@@ -20,11 +20,11 @@ choice3D = input('Will you be watching this film in 3D? [Y/n] \n')
 # 'age' is the users age
 age = eval(input('How old are you?\n'))
 # This says if the choice 3D is equal to 'Y' or yes, then run lines 24-31
-if choice3D == 'Y' or choice3D == 'y':
+if choice3D == 'Y' or choice3D == 'y' or choice3D == 'yes' or choice3D == 'Yes':
 # Define ticket3D as the price of the flat ticket rate plus the surcharge
     ticket3D = ticketFlat + surcharge3D
 # If the variable 'age' is less than 12, or greater than 60, then the age discount is applied in line 28
-    if age < 12 or age > 60:
+    if age < 12 or age >= 60:
         ticketPrice = ticket3D - (ticket3D * ageDiscount)
 # If the variable 'age' falls outside of this realm, the user is not eligable for an age-based discount
     else:
@@ -32,9 +32,9 @@ if choice3D == 'Y' or choice3D == 'y':
 # This conditional runs if the user does not plan on watching a movie in 3D
 else: 
 # The same age conditional as before, but with the variable 'ticketFlat' instead of 'ticket3D' 
-    if age < 12 or age > 60:
+    if age < 12 or age >= 60:
         ticketPrice = ticketFlat - (ticketFlat * ageDiscount)
     else:
         ticketPrice = ticketFlat    
 # Finally, print the final ticket price
-print ('ticket price: ' + str(ticketPrice))
+print ('Your ticket price is $' + str(ticketPrice))
