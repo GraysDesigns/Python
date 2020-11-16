5811050a3094b28caa1e2f814cf1e23a19cca3d0
Richard Gray <richardgray@richards-air.lan>
2020-11-16 18:03:28 -0500

    pushing with assignment 3

---
+++ Assignment3.py
@@ -0,0 +1,52 @@
+# Assignment
+# -----------
+# Write a Python program to compute the cost of carpeting a room. 
+# Prompt the user for the width and height in feet of the room and the quality of carpet to be used. 
+# A choice between three grades of carpeting should be given. You should decide on the price per square foot of the three grades on carpet. 
+# Your program must include a function that accepts the height, width, and carpet quality as parameters and returns the cost of carpeting that room. 
+# After calling that function, your program should then output the carpeting cost.
+# -----------
+# CODE
+# -----------
+
+# User inputs length and width as integers, and the quality of carpet they'd like to use as a string
+length = eval(input("What is the length of space in feet? \n"))
+width = eval(input("What is the width of space in feet? \n"))
+qual = input("Which Quality of carpet would you like? (ex. Economy, Midgrade, Luxury)\n")
+
+# The function qualCost() takes the string input from the user and converts it into the price per square foot of carpeting, this becomes a multiplier later on. You can see that there are many "or" functions to accomidate for various kinds of inputs that are outside the realm of the example
+def qualCost(q):
+# if the user selects the economy grade carpet, the price is $0.70/sqft
+    while q == False:
+        if q == "economy" or q == "Economy" or q == "econ":
+            return .7
+# if the user selects the midgrade carpet, the price is $1.50/sqft
+        elif q == "Midgrade" or q == "midgrade" or q =="mid":
+            return 1.5
+# if the user selects the midgrade carpet, the price is $2.10/sqft
+        elif q == "Luxury" or q == "luxury" or q == "lux":
+            return 2.1
+        else:
+            return False    
+# NOTE: dollar amounts/sqft of carpet are taken from current lowes pricing
+
+
+# Next we create a sqft() definition, sqft() takes two parameters (length and width) and multiplies them to get the total square footage of the area to be covered by carpet
+def sqft(l, w):
+    return (l*w)
+
+# The Dollar() function says if the dollar amount passed in is a whole number (modulus 0 i.e. no remainder) then it returns an integer with no float, otherwise, the cost is rounded to the nearest hundredth decimal place, as is how monetary values are usually given. 
+def Dollar(d):
+    if d % 1 == 0:
+        return int(d)
+    else:
+        return round(d, 2)    
+
+# This is our total cost function. It takes the Dollar function and passes through the square footage function (sqft()) and multiplies it by the qualCost() function to get the total price of the carpet to be sold. 
+
+totalCost = Dollar((sqft(length, width) * qualCost(qual)))
+
+# The functions then need to be passed as strings to be printable
+print ("You are buying " + str(sqft(length, width)) + " sq.ft. of carpet at $" + str(Dollar(qualCost(qual))) + " per sq.ft.")
+
+print ("Your total cost today is $" + str(totalCost))
--- Discussion4Gray.py
+++ Discussion4Gray.py
@@ -7,8 +7,28 @@
 # Also post a screen shot of executing your program on at least one test case.
 # -----------
 # DESCRIPTION
-# 
+# Pipe ID, OD calculator: This program calculates the inner area and outer circumfrence of a pipe, given the overall diameter of the outside of the pipe and the material's thickness.
 # -----------
 # CODE
 # -----------
+# Imports the Math library
+import math
+# User inputs the outer diameter of the pipe in cm, the value is logged as pipeOD
+pipeOD = eval(input("What is the outer diameter of the pipe in cm?\n"))
+# User inputs the thickness of the material in cm, the value is logged as pipeT
+pipeT = eval(input("What is the materials thickness in cm?\n"))
+# Calculate the inner diameter (pipeID) of the pipe and the inner radius (pipeIR) of the pipe (radius is d/2)
+pipeID = pipeOD - pipeT
+pipeIR = pipeID/2
+# Calculate the area of the inside of the pipe using A = pi(r)^2, log as pipeIArea
+pipeIArea = round(math.pi * (pipeIR * pipeIR), 2)
+# Calculate the outer circumfrence of the pipe using C = pi(d), log as pipeOC
+pipeOC = round(math.pi * (pipeOD), 2)
 
+# If pipeID is less than 0, it is not a pipe, there is no inner diameter.
+if pipeID > 0:
+    # Print the values in a digestable format, be sure to include str() function so that the print() function can properly parse out float types.
+    print("The outer circumfrence of the pipe is: \n" + str(pipeOC) + "cm.")
+    print("The inner area of the pipe is: \n" + str(pipeIArea) + "cm^2.")
+else:
+    print("This is not a pipe.")
