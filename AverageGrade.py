231e0a5e44a0bbb7c15cf1f038a6817122c76756
Richard Gray <richardgray@richards-air.lan>
2020-11-21 18:12:49 -0500

    added average grade.py

--- AverageGrade.py
+++ AverageGrade.py
@@ -9,3 +9,23 @@
 # ---------
 # CODE 
 # ---------
+
+# Define a list with all the names of the students.
+students = ['Tim', 'Kate', 'Igor', 'Beth', 'Jonny']
+# Define an empty list that will be populated with the grades
+grades = []
+# populate the list by asking what each students grade was
+for i in students:
+    grades.append(eval(input("What was " + i + "'s Grade: ")))
+
+# use a simple sum for loop to calculate the total sum of all grades
+total = 0
+for i in grades:
+    total += i
+
+# to get the average of the grades, the total is divided by len(students) (or 5)
+avg = total/len(students)
+
+# print the average
+print("The average score was:", avg)
+
